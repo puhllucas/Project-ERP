19c0f3a0417dbc4f8c91ffc89dc5b4ecd1303d6d
lucaspuhl <lucaaspuhl@gmail.com>
2022-12-04 20:04:07 -0300

    database updated

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ERP_APP',
+        'USER': 'pullucas',
+        'PASSWORD': 'LUCAS.exe2301',
+        'HOST': '192.168.0.2',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
