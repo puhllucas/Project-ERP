800f38d0b23feb506f7dbb75ed7b874a6637cd97
lucaspuhl <lucaaspuhl@gmail.com>
2022-12-14 18:10:22 -0300

    CRUD Created

--- ERP/App/views.py
+++ ERP/App/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Apps
 from .forms import AppForms
 
@@ -17,3 +17,26 @@ def home(request):
             form = AppForms()
             
     return render(request, 'index.html', {'data': data})
+
+
+def edit(request, id) -> str:
+    data = get_object_or_404(Apps, pk=id)
+    form = AppForms(instance=data)
+
+    if request.method == 'POST':
+        form = AppForms(request.POST, instance=data)
+
+        if form.is_valid():
+            data.save()
+            return redirect('/')
+        else:
+            return render(request, 'edit.html', {'data': data})
+
+    else:
+        return render(request, 'edit.html', {'data': data})
+
+
+def delete(request, id) -> str:
+    data = get_object_or_404(Apps, pk=id)
+    data.delete()
+    return redirect('/')
